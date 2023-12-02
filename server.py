bf38a063bde21f761c114efdc95467c51ac83e1e
MNGR <93889231+0120MNGR@users.noreply.github.com>
2023-12-02 15:01:31 +0000

    [fix] requirements.txtの追加

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import json
 app = Flask(__name__)
 
 global model
-print('ファイル読み込み中のため、接続しないでください。')
+print('ファイル読み込み中のため、クライアントを接続しないでください。')
 model = gensim.models.KeyedVectors.load_word2vec_format('model_nouns_only.vec', binary=False)
 print('読み込み完了')
 
@@ -18,8 +18,6 @@ def similar():
     try:
         similar_words = model.most_similar(positive=[word])
         words_text = similar_words[0][0] + " " + word
-        # response_data = {'similar_words': word_lst}
-        # response_json = json.dumps(response_data, ensure_ascii=False)
         return Response(words_text, content_type="text/plain; charset=utf-8")
 
     except KeyError:
--- test_client.py
+++ test_client.py
@@ -3,7 +3,7 @@ import requests
 def get_similar_words(word):
     response = requests.get(f"http://localhost:5000/similar", params={'word': word})
     if response.status_code == 200:
-        return response.json()
+        return response.text
     else:
         return {'error': f'Error: {response.status_code}'}
 
