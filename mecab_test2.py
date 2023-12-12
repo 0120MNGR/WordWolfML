429a48acbbcf1dbfc7b65ec44959f866ce42cece
MNGR <93889231+0120MNGR@users.noreply.github.com>
2023-12-12 22:55:17 +0900

    Update 途中経過

---
+++ mecab_test2.py
@@ -0,0 +1,3 @@
+import MeCab
+
+mecab = MeCab.Tagger()
--- meishi.py
+++ meishi.py
@@ -1,45 +1,12 @@
-# import MeCab
-# import re
-# from tqdm import tqdm
-
-
-
-# # MeCab の初期化
-# mecab = MeCab.Tagger()
-
-# # 元の .vec ファイルを開く
-# with open('model.vec', 'r', encoding='utf-8') as file:
-#     lines = file.readlines()
-
-# # 新しい .vec ファイル用のデータを格納
-# new_lines = []
-
-# for line in tqdm(lines):
-#     # 単語を抽出
-#     word = line.split(' ')[0]
-
-#     # 形態素解析を使用して品詞を取得
-#     node = mecab.parseToNode(word)
-#     features = node.next.feature.split(',')
-
-#     # 品詞が名詞であれば、リストに追加
-#     if features[0] == '名詞':
-#         new_lines.append(line)
-
-# # 新しい .vec ファイルにデータを保存
-# with open('model_nouns_only.vec', 'w', encoding='utf-8') as file:
-#     for line in tqdm(new_lines):
-#         file.write(line)
-
 import MeCab
-import re
+import ipadic
 from tqdm import tqdm
 
 # MeCabの初期化
-mecab = MeCab.Tagger()
+mecab = MeCab.Tagger(ipadic.MECAB_ARGS)
 
 # 元の.vecファイルを開く
-with open('model_neologd.vec', 'r', encoding='utf-8') as file:
+with open('model_nouns_only.vec', 'r', encoding='utf-8') as file:
     # ヘッダー行を読み込む
     header = file.readline()
     # ファイルの残りの部分を読み込む
@@ -51,13 +18,14 @@ new_lines = []
 for line in tqdm(lines):
     # 単語を抽出
     word = line.split(' ')[0]
-
+    # print(word)
     # 形態素解析を使用して品詞を取得
     node = mecab.parseToNode(word)
     while node:
         features = node.feature.split(',')
         # 品詞が名詞であれば、リストに追加
-        if features[0] == '名詞':
+        if features[0] == '名詞' and features[1] == "一般":
+            # print(word, features)
             new_lines.append(line)
             break
         node = node.next
@@ -66,8 +34,9 @@ for line in tqdm(lines):
 num_words = len(new_lines)
 
 # 新しい.vecファイルにデータを保存
-with open('model_neo_nouns_only.vec', 'w', encoding='utf-8') as file:
+with open('./noun_vec/m_ippan.vec', 'w', encoding='utf-8') as file:
     # 更新されたヘッダー行を書き込む
     file.write(f"{num_words} {header.split(' ')[1]}")
     for line in new_lines:
         file.write(line)
+
